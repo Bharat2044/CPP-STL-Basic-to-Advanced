874f3fd1719c4d1c7e745915477ffe3c1687b7a9
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-05-01 01:30:12 +0530

    array declaration and initialization code added

--- 02_STL/02_Sequence Containers/01_Arrays/01_declarationAndInitialization.cpp
+++ 02_STL/02_Sequence Containers/01_Arrays/01_declarationAndInitialization.cpp
@@ -5,21 +5,26 @@ using namespace std;
 int main() {
 	array<int, 5> arr1;    // declaration
 	array<int, 6> arr2 = {1, 2, 3, 4, 5, 6};    // declaration + initialization
+	array<int, 6> arr3{6, 7, 8, 9, 10, 11};	    // declaration + initialization
 
     // printing elements
-    cout << "arr1 elements are: ";
+    cout << "Array arr1 elements are: ";
     for(int i=0; i<5; i++)
         cout << arr1[i] << "  ";
     cout << endl;
-    
-    // printing elements
-    cout << "\narr2 elements are: ";
+	
+    cout << "\nArray arr2 elements are: ";
     for(int i=0; i<6; i++)
         cout << arr2.at(i) << "  ";
     cout << endl;
     
     for(int i=0; i<10; i++)
         cout << arr2[i] << "  ";    // not bound checking here
+    cout << endl;
+    
+    cout << "\nArray arr3 elements are: ";
+    for(auto ele : arr3)
+        cout << ele << "  ";
     cout << endl;
     
     // for(int i=0; i<10; i++)      //     // not bound checking here
@@ -32,17 +37,15 @@ int main() {
     cout << "\nEnter size of the array: ";
     cin >> n;
     
-    array<int, 100> arr3;
-    cout << "Enter array arr3 elements:\n";
+    array<int, 100> arr4;
+    cout << "Enter array arr4 elements:\n";
     for(int i=0; i<n; i++) 
-        cin >> arr3[i];
+        cin >> arr4[i];
         
-    cout << "\narr3 elements are: ";
+    cout << "\nArray arr4 elements are: ";
     for(int i=0; i<n; i++)
-        cout << arr3.at(i) << "  ";
+        cout << arr4.at(i) << "  ";
     cout << endl;
-    
 
 	return 0;
-
 }
