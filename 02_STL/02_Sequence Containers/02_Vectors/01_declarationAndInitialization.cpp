90fd9efb19e1b99b33d1032fdf0ffad8aa7fa265
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-05-01 18:53:46 +0530

    vector declaration and initialization code added

--- 02_STL/02_Sequence Containers/02_Vectors/01_declarationAndInitialization.cpp
+++ 02_STL/02_Sequence Containers/02_Vectors/01_declarationAndInitialization.cpp
@@ -20,10 +20,18 @@ int main() {
     vector<int> v5{1, 2, 6, 4};
     vector<int> v6 = {1, 2, 3, 4, 5, 7};
     
-    // Copy array elements into vector
+    // Initializing from another vector 
+    vector<int> v7(v6.begin(), v6.end());
+    
+    // Initializing from an array
     int arr[] = {10, 5, 20, 45};
     int size = sizeof(arr)/sizeof(arr[0]);
-    vector<int> v7(arr, arr+size);
+    vector<int> v8(arr, arr+size);
+    
+    // Initializing all elements with a particular value :
+    vector<int> v9(10);
+    int value = 5;
+    fill(v9.begin(), v9.end(), value);
 
     // Printing Vector Size  
     cout << "--------- Printing Vector Size: ---------\n";
@@ -32,8 +40,10 @@ int main() {
     cout << "Size of v3 = " << v3.size() << endl;
     cout << "Size of v4 = " << v4.size() << endl;
     cout << "Size of v5 = " << v5.size() << endl;
-    cout << "Size of v5 = " << v6.size() << endl;
+    cout << "Size of v6 = " << v6.size() << endl;
     cout << "Size of v7 = " << v7.size() << endl;
+    cout << "Size of v8 = " << v8.size() << endl;
+    cout << "Size of v9 = " << v9.size() << endl;
 
 
     // Printing Vector Elements
@@ -59,23 +69,29 @@ int main() {
     cout << "Vector v7 elements are: ";
     print(v7);
     
+    cout << "Vector v8 elements are: ";
+    print(v8);
+    
+    cout << "Vector v9 elements are: ";
+    print(v9);
+    
     // Taking Input From User
     int n;
     cout << "\nEnter size of vector: ";
     cin >> n;
     
-    vector<int> v8;
+    vector<int> v10;
     cout << "Enter vector elements:\n";
     for(int i=0; i<n; i++) {
         int x;
         cin >> x;
-        v8.push_back(x);
+        v10.push_back(x);
     }
     
-    cout << "Vetcor v8 elements are: ";    
-    print(v8);
-    cout << "First element of v8 = " << v8.front() << endl;
-    cout << "First element of v8 = " << v8.back() << endl;
+    cout << "Vetcor v10 elements are: ";    
+    print(v10);
+    cout << "First element of v10 = " << v10.front() << endl;
+    cout << "First element of v10 = " << v10.back() << endl;
 
     return 0;
 }
